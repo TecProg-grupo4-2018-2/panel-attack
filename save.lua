63c3a366bdeaad563072d125e57dd8a79d4adafa
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-10-25 18:22:18 -0300

    Improve save.lua readability

--- save.lua
+++ save.lua
@@ -20,14 +20,18 @@ function read_key_file() pcall(function()
   -- load user config
   local user_conf = json.decode(teh_json)
   file:close()
+
   -- TODO: remove this later, it just converts the old format.
   if #user_conf == 0 then
     local new_conf = {}
+
     for k,v in pairs(user_conf) do
       new_conf[k:sub(3)] = v
     end
+
     user_conf = {new_conf, {}, {}, {}}
   end
+
   for k,v in ipairs(user_conf) do
     keyboard[k]=v
   end
@@ -37,18 +41,21 @@ end) end
 function read_txt_file(path_and_filename)
   assert(path_and_filename)
   local file_size
+
   pcall(function()
     local file = love.filesystem.newFile(path_and_filename)
     file:open("r")
     file_size= file:read(file:getSize())
     file:close()
   end)
+
   if not file_size then
     file_size  = "Failed to read file"..path_and_filename
   else
     -- substitute multiple newlines for one newline
     file_size = file_size:gsub('\r\n?', '\n')
   end
+
   return file_size or "Failed to read file"
  end
 
@@ -65,9 +72,11 @@ function read_conf_file() pcall(function()
   local file = love.filesystem.newFile("conf.json")
   file:open("r")
   local teh_json = file:read(file:getSize())
+
   for k,v in pairs(json.decode(teh_json)) do
     config[k] = v
   end
+
   file:close()
 end) end
 
@@ -77,6 +86,7 @@ function read_replay_file() pcall(function()
   file:open("r")
   local teh_json = file:read(file:getSize())
   replay = json.decode(teh_json)
+
   if type(replay.in_buf) == "table" then
     replay.in_buf=table.concat(replay.in_buf)
     write_replay_file()
@@ -86,6 +96,7 @@ end) end
 -- write replay file to default path
 function write_replay_file() pcall(function()
   local file = love.filesystem.newFile("replay.txt")
+
   file:open("w")
   file:write(json.encode(replay))
   file:close()
@@ -95,6 +106,7 @@ end) end
 function write_replay_file(path, filename) pcall(function()
   assert(path)
   assert(filename)
+
   love.filesystem.createDirectory(path)
   local file = love.filesystem.newFile(path.."/"..filename)
   file:open("w")
@@ -102,10 +114,12 @@ function write_replay_file(path, filename) pcall(function()
   file:close()
 end) end
 
+server_path = "servers/"..connected_server_ip
 -- write user id file
 function write_user_id_file() pcall(function()
-  love.filesystem.createDirectory("servers/"..connected_server_ip)
-  local file = love.filesystem.newFile("servers/"..connected_server_ip.."/user_id.txt")
+  love.filesystem.createDirectory(server_path)
+  local file = love.filesystem.newFile(server_path.."/user_id.txt")
+
   file:open("w")
   file:write(tostring(my_user_id))
   file:close()
@@ -113,19 +127,12 @@ end) end
 
 -- read user id file
 function read_user_id_file() pcall(function()
-  local file = love.filesystem.newFile("servers/"..connected_server_ip.."/user_id.txt")
+  local file = love.filesystem.newFile(server_path.."/user_id.txt")
+
   file:open("r")
   my_user_id = file:read()
   file:close()
 end) end
-
--- this function is never called
-function print_list(t)
-  assert(t)
-  for i, v in ipairs(t) do
-    print(v)
-  end
-end
 
 -- copy recursively from files in source
 function recursive_copy(source, destination)
@@ -134,15 +141,19 @@ function recursive_copy(source, destination)
   local lfs = love.filesystem
   local names = lfs.getDirectoryItems(source)
   local temp
+
   for i, name in ipairs(names) do
+
     if lfs.isDirectory(source.."/"..name) then
       print("calling recursive_copy(source".."/"..name..", ".. destination.."/"..name..")")
       recursive_copy(source.."/"..name, destination.."/"..name)
 
     elseif lfs.isFile(source.."/"..name) then
+
       if not lfs.isDirectory(destination) then
        love.filesystem.createDirectory(destination)
       end
+
       print("copying file:  "..source.."/"..name.." to "..destination.."/"..name)
 
       local source_file = lfs.newFile(source.."/"..name)
