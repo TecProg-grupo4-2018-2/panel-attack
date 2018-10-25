287d6da7886e3e6a5ab0b37994ffb1fd13f2d840
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-10-25 18:40:45 -0300

    Improve gen_panels readability

--- gen_panels.lua
+++ gen_panels.lua
@@ -2,26 +2,28 @@ require("util")
 require("server_globals")
 local random = math.random
 
--- stuff should have first_seven, metal, vs_mode, metal_col, prev_metal_col
+--  stuff should have first_seven, metal, vs_mode, metal_col, prev_metal_col
 function make_panels(ncolors, prev_panels, stuff)
-  --print("make_panels("..ncolors..", "..prev_panels..", "..(stuff.first_seven or "")..")")
+  -- print("make_panels("..ncolors..", "..prev_panels..", "..(stuff.first_seven or "")..")")
   assert(type(ncolors) == 'number')
   assert(prev_panels, 'Null prev_panels')
   local ret = prev_panels
-  local rows_to_make = 20
-  local rows_to_place_metal_locations = rows_to_make
-  if ncolors < 2 then return end
+  local ROWS_TO_MAKE = 20
+
+  if ncolors < 2 then return nil end
+
   local cut_panels = false
 
   if prev_panels == "000000" then
     if stuff.first_seven then
       ret = stuff.first_seven
-      rows_to_make = rows_to_make - 7
+      ROWS_TO_MAKE = ROWS_TO_MAKE - 7
     elseif stuff.vs_mode or stuff.mode == "vs" then
       cut_panels = true
     end
   end
-  for x=0,rows_to_make-1 do
+
+  for x=0,ROWS_TO_MAKE-1 do
     for y=0,5 do
       local prevtwo = y>1 and string.sub(ret,-1,-1) == string.sub(ret,-2,-2)
       local nogood,color = true
@@ -34,29 +36,29 @@ function make_panels(ncolors, prev_panels, stuff)
       ret = ret..color
     end
   end
-  --print("panels before potential metal panel position assignments:")
-  --print(ret)
-  --assign potential metal panel placements
-  local row_width = 6 --this may belong in globals if we were to ever make a game mode with a different width
+  -- print("panels before potential metal panel position assignments:")
+  -- print(ret)
+  -- assign potential metal panel placements
+  local ROW_WIDTH = 6 -- this may belong in globals if we were to ever make a game mode with a different width
   local new_ret = "000000"
   local new_row
   local prev_row
-  for i=2,rows_to_place_metal_locations+1 do
-    current_row_from_ret =  string.sub(ret,(i-1)*row_width+1, (i-1)*row_width+row_width)
-    --print("current_row_from_ret: "..current_row_from_ret)
-    if tonumber(current_row_from_ret) then --doesn't already have letters in it for metal panel locations
-      prev_row = string.sub(new_ret,0-row_width,-1)
-      local first, second --locations of potential metal panels
-      --while panel vertically adjacent is not numeric, so can be a metal panel 
-      while not first or not tonumber(string.sub(prev_row, first, first)) do 
-        first = math.random(1,row_width)
+  for i=2,ROWS_TO_MAKE+1 do
+    current_row_from_ret =  string.sub(ret,(i-1)*ROW_WIDTH+1, (i-1)*ROW_WIDTH+ROW_WIDTH)
+    -- print("current_row_from_ret: "..current_row_from_ret)
+    if tonumber(current_row_from_ret) then -- doesn't already have letters in it for metal panel locations
+      prev_row = string.sub(new_ret,0-ROW_WIDTH,-1)
+      local first, second -- locations of potential metal panels
+      -- while panel vertically adjacent is not numeric, so can be a metal panel
+      while not first or not tonumber(string.sub(prev_row, first, first)) do
+        first = math.random(1,ROW_WIDTH)
       end
-      while not second or second==first or not tonumber(string.sub(prev_row, second, second)) do 
-        second = math.random(1,row_width)
+      while not second or second==first or not tonumber(string.sub(prev_row, second, second)) do
+        second = math.random(1,ROW_WIDTH)
       end
       new_row = ""
-      for j=1, row_width do
-        local chr_from_ret = string.sub(ret,(i-1)*row_width+j, (i-1)*row_width+j)
+      for j=1, ROW_WIDTH do
+        local chr_from_ret = string.sub(ret,(i-1)*ROW_WIDTH+j, (i-1)*ROW_WIDTH+j)
         local num_from_ret = tonumber(chr_from_ret)
         if j==first then
           new_row = new_row..(panel_color_number_to_upper[num_from_ret] or chr_from_ret or "0")
@@ -72,8 +74,8 @@ function make_panels(ncolors, prev_panels, stuff)
     new_ret = new_ret..new_row
   end
   ret = new_ret
-  --print("panels after potential metal panel position assignments:")
-  --print(ret)
+  -- print("panels after potential metal panel position assignments:")
+  -- print(ret)
   if cut_panels then
     ret = procat(ret)
     local height = {7,7,7,7,7,7}
@@ -101,8 +103,10 @@ function make_gpanels(ncolors, prev_panels)
   assert(type(ncolors) == 'number')
   assert(prev_panels)
   local ret = prev_panels
-  for x=0,19 do
-    for y=0,5 do
+  local X_LENGTH = 19
+  local Y_LENGTH = 5
+  for x=0,X_LENGTH do
+    for y=0,Y_LENGTH do
       local nogood,color = true
       while nogood do
         color = tostring(math.random(1,ncolors))
