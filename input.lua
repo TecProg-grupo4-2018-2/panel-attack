24e3a4889966be5fce5520dfb56c02adafbb6195
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-10-25 18:24:14 -0300

    Improve input.lua readability

--- input.lua
+++ input.lua
@@ -1,9 +1,11 @@
 local jpexists, jpname, jrname
+
 for k,v in pairs(love.handlers) do
   if k=="jp" then
     jpexists = true
   end
 end
+
 if jpexists then
   jpname = "jp"
   jrname = "jr"
@@ -11,32 +13,38 @@ else
   jpname = "joystickpressed"
   jrname = "joystickreleased"
 end
+
 local __old_jp_handler = love.handlers[jpname]
 local __old_jr_handler = love.handlers[jrname]
+
 love.handlers[jpname] = function(a, b)
   __old_jp_handler(a,b)
   love.keypressed("j"..a:getID()..b)
 end
+
 love.handlers[jrname] = function(a,b)
   __old_jr_handler(a,b)
   love.keyreleased("j"..a:getID()..b)
 end
 
 local prev_ax = {}
+
 local axis_to_button = function(idx, value)
   local prev = prev_ax[idx] or 0
-  if value > .5 then
-    if prev < .5 then
+  local THRESHOLD = .5
+
+  if value > THRESHOLD then
+    if prev < THRESHOLD then
       love.keypressed("ja"..idx.."+")
     end
-  elseif value < -.5 then
-    if prev > -.5 then
+  elseif value < - THRESHOLD then
+    if prev > -THRESHOLD then
       love.keypressed("ja"..idx.."-")
     end
   else
-    if prev > .5 then
+    if prev > THRESHOLD then
       love.keyreleased("ja"..idx.."+")
-    elseif prev < -.5 then
+    elseif prev < - THRESHOLD then
       love.keyreleased("ja"..idx.."-")
     end
   end
@@ -44,7 +52,9 @@ local axis_to_button = function(idx, value)
 end
 
 local prev_hat = {{},{}}
+
 local hat_to_button = function(idx, value)
+
   if string.len(value) == 1 then
     if value == "l" or value == "r" then
       value = value .. "c"
@@ -52,15 +62,20 @@ local hat_to_button = function(idx, value)
       value = "c" .. value
     end
   end
+
   value = procat(value)
+
   for i=1,2 do
     local prev = prev_hat[i][idx] or "c"
+
     if value[i] ~= prev and value[i] ~= "c" then
       love.keypressed("jh"..idx..value[i])
     end
+
     if prev ~= value[i] and prev ~= "c" then
       love.keyreleased("jh"..idx..prev)
     end
+
     prev_hat[i][idx] = value[i]
   end
 end
@@ -68,21 +83,26 @@ end
 function love.joystick.getHats(joystick)
   local n = joystick:getHatCount()
   local ret = {}
+
   for i=1,n do
     ret[i] = joystick:getHat(i)
   end
+
   return unpack(ret)
 end
 
 function joystick_ax()
   local joysticks = love.joystick.getJoysticks()
+
   for k,v in ipairs(joysticks) do
     local axes = {v:getAxes()}
+
     for idx,value in ipairs(axes) do
       axis_to_button(k..idx, value)
     end
 
     local hats = {love.joystick.getHats(v)}
+
     for idx,value in ipairs(hats) do
       hat_to_button(k..idx, value)
     end
@@ -92,11 +112,13 @@ end
 function love.keypressed(key, rep)
   if key == "return" and not rep and love.keyboard.isDown("lalt") then
     love.window.setFullscreen(not love.window.getFullscreen(), "desktop")
-    return
+    return nil
   end
+
   if not rep then
     keys[key] = 0
   end
+
   this_frame_keys[key] = true
 end
 
@@ -104,7 +126,7 @@ function love.textinput(text)
   this_frame_unicodes[#this_frame_unicodes+1] = text
 end
 
-function love.keyreleased(key, unicode)
+function love.keyreleased(key)
   keys[key] = nil
 end
 
@@ -118,6 +140,7 @@ function Stack.controls(self)
   local new_dir = nil
   local sdata = self.input_state
   local raise, swap, up, down, left, right = unpack(base64decode[sdata])
+
   if (raise) and (not self.prevent_manual_raise) then
     self.manual_raise = true
     self.manual_raise_yet = false
