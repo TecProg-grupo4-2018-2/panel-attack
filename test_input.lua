e8df4521664e26eb51cff0926fcf80f53afc7255
bumbleblo <bumbleblo2013@gmail.com>
2018-11-30 10:41:03 -0200

    #131 - add a testcase

---
+++ test_input.lua
@@ -0,0 +1,26 @@
+require("lunit")
+
+module("input_test", lunit.testcase)
+
+local input = require 'input.lua'
+
+function test_pass_jp_name()
+    assert_true(input.set_jpname(true) == 'jp')
+    assert_true(input.set_jrname(false) == 'joystickpressed')
+end
+
+function test_fail_jp_name()
+    assert_false(input.set_jpname(false) == 'jp')
+    assert_false(input.set_jpname(true) == 'joystickpressed')
+end
+
+
+function test_pass_set_jrname
+    assert_true(input.set_jrname(true) == 'jr')
+    assert_true(input.set_jrname(false) == 'joystickreleased')
+end
+
+function test_fail_set_jrname
+    assert_false(input.set_jrname(false) == 'jr')
+    assert_false(input.set_jrname(true) == 'joystickreleased')
+end
